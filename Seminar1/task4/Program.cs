9c26c389cb4c3408283a6018b353dcfd9062c6ed
Oleg Kalinin <o.a.kalinin@gmail.com>
2022-09-05 22:58:18 +0300

    Realized task 4, 6, 8

---
+++ Seminar1/task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Задайте значения для трех чисел - a, b, с");
+
+Console.Write("Задайте значения a: ");
+int number_a= Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте значения b: ");
+int number_b= Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте значения с: ");
+int number_c= Convert.ToInt32(Console.ReadLine());
+
+ Console.Write("Максимальное из веденных чисел - ");
+ Console.WriteLine(Math.Max(Math.Max(number_a, number_b), number_c));
---
+++ Seminar1/task8/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Задайте значение числа (число должно быть > 0 : ");
+int number_a= Convert.ToInt32(Console.ReadLine());
+string result = "";
+if (number_a > 0)
+{
+    for (int i = 1; i <= number_a; i++)
+    {
+        if (i % 2 == 0)
+            {
+               result = " " +result + Convert.ToString(i) + ", ";
+            }
+    }
+
+    Console.Write("Все четные числа от 0 до ");
+    Console.WriteLine(number_a);
+    Console.WriteLine(result.Trim(new char[] { ' ', ',' }));
+} 
+else
+{
+    Console.Write("Введено число, не отвечающее заданному условию.");
+}
