d863485b2e368f1a75cd0d1f83db7d7407c2a75c
Oleg Kalinin <o.a.kalinin@gmail.com>
2022-09-10 20:05:36 +0300

    Realized tasks 19, 21, 23 from seminar 3

---
+++ Seminar3/Task19/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string s="";
+if (Number >= 10000 && Number < 100000)
+{
+    
+    for (int i=Convert.ToString(Number).Length; i>0; --i)
+    {     
+        s=s+Convert.ToString(Number)[i-1];   
+    } 
+    
+    if (Number==Convert.ToInt32(s))
+    {
+        Console.WriteLine("Это палиндром.");
+        }
+    else
+    {
+        Console.WriteLine("Это не палиндром.");
+    } 
+}
+else
+{
+    Console.WriteLine("Введено не пятизначное число.");
+}
---
+++ Seminar3/Task21/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты точки А:");
+Console.Write("  координаты точки А по оси x: ");
+int Ax = Convert.ToInt32(Console.ReadLine());
+Console.Write("  координаты точки А по оси y: ");
+int Ay = Convert.ToInt32(Console.ReadLine());
+Console.Write("  координаты точки А по оси z: ");
+int Az = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B:");
+Console.Write("  координаты точки B по оси x: ");
+int Bx = Convert.ToInt32(Console.ReadLine());
+Console.Write("  координаты точки B по оси y: ");
+int By = Convert.ToInt32(Console.ReadLine());
+Console.Write("  координаты точки B по оси z: ");
+int Bz = Convert.ToInt32(Console.ReadLine());
+
+if (Bx!=Ax && By!=Ay && Bz!=Az)
+{
+   Console.WriteLine($"Расстояние между заданными точками: {Math.Sqrt(Math.Pow(Bx-Ax,2)+Math.Pow(By-Ay,2)+Math.Pow(Bz-Az,2)):f2}"); 
+}
+else 
+{
+    Console.WriteLine("Точки совпадают.");
+}
