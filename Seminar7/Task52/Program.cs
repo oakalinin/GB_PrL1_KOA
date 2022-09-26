5be3d3561faf99636a662adb57af78ebcda7546a
Oleg Kalinin <o.a.kalinin@gmail.com>
2022-09-26 18:37:36 +0300

    Realized tasks 47, 50, 52 from seminar 7 (final).

--- Seminar7/Task52/Program.cs
+++ Seminar7/Task52/Program.cs
@@ -12,7 +12,7 @@ int[,] GetArr(int size_1 = 3, int size_2=4, int minValue=-10, int maxValue=10)
     for (int i = 0; i<size_1; i++)
      for (int j=0; j<size_2; j++)
      {
-        //int.TryParse(Console.ReadLine(), out temp_arr[i,j]); // Раскоментировать эту строку, если нужен ручной ввод значений массива
+        //int.TryParse(Console.ReadLine(), out temp_arr[i,j]); // Раскоментировать эту строку и закоментировать строку 16, если нужен ручной ввод значений массива
         temp_arr[i,j]=rand.Next(minValue, maxValue+1);
      }
      return temp_arr;
